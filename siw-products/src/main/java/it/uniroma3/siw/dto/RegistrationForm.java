3b3bfb1445655f3300e83da328235b63ebab26ea
danieleene <91674916+danieleene@users.noreply.github.com>
2025-09-22 10:35:21 +0200

    Update RegistrationForm.java
    Aggiunto il costruttore vuoto NECESSARIO
    
    Aggiunti i getter & setter anche per il campo 'Ruolo' in seguito al successo della corretta registrazione dell'utente con il corretto ruolo.

--- siw-products/src/main/java/it/uniroma3/siw/dto/RegistrationForm.java
+++ siw-products/src/main/java/it/uniroma3/siw/dto/RegistrationForm.java
@@ -8,6 +8,9 @@ public class RegistrationForm {
     private String email;
     private String password;
 
+    //Costruttore vuoto
+    public RegistrationForm() {}
+    
     // Getter e Setter
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
@@ -20,4 +23,9 @@ public class RegistrationForm {
 
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
+
+    public String getRole() {return role; }
+	public void setRole(String role) {this.role = role; }
+	
 }
+
