04773e53fe8483116761978827b297b776532814
danieleene <91674916+danieleene@users.noreply.github.com>
2025-09-23 20:13:50 +0200

    Update CommentoRepository.java
    E' stato esteso con 3 nuovi metodi verranno richiamati in CommentoService! Commit a seguire.

--- siw-products/src/main/java/it/uniroma3/siw/repository/CommentoRepository.java
+++ siw-products/src/main/java/it/uniroma3/siw/repository/CommentoRepository.java
@@ -6,5 +6,9 @@ import it.uniroma3.siw.model.Commento;
 
 
 public interface CommentoRepository extends CrudRepository<Commento, Long> {
-    
+   Optional<Commento> findByAutoreAndProdotto(Utente autore, Prodotto prodotto);
+
+    void deleteByAutoreAndProdotto(Utente autore, Prodotto prodotto);
+    List<Commento> findByProdotto(Prodotto prodotto);
 }
+
