aa9acb78600ace355b4e8642f47e35bd40e7f1ff
danieleene <91674916+danieleene@users.noreply.github.com>
2025-09-23 20:14:37 +0200

    Update CredenzialiRepository.java

--- siw-products/src/main/java/it/uniroma3/siw/repository/CredenzialiRepository.java
+++ siw-products/src/main/java/it/uniroma3/siw/repository/CredenzialiRepository.java
@@ -7,4 +7,7 @@ import it.uniroma3.siw.model.Credenziali;
 
 public interface CredenzialiRepository extends CrudRepository<Credenziali, Long> {
     Credenziali findByEmail(String email);
+     @Query("SELECT c FROM Credenziali c JOIN FETCH c.utente WHERE c.username = :username")
+    Credenziali findByUsername(@Param("username") String username);
 }
+
