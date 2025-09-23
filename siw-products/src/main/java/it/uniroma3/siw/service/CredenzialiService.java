60c9751f3eee856c10e5b55f4c480e214175da52
danieleene <91674916+danieleene@users.noreply.github.com>
2025-09-23 20:18:02 +0200

    Update CredenzialiService.java
    E' stato aggiunto il metodo getter

--- siw-products/src/main/java/it/uniroma3/siw/service/CredenzialiService.java
+++ siw-products/src/main/java/it/uniroma3/siw/service/CredenzialiService.java
@@ -33,5 +33,9 @@ public class CredenzialiService {
     public void deleteById(Long id) {
         credenzialiRepository.deleteById(id);
     }
+    public Credenziali getCredenziali(String username) {
+    return credenzialiRepository.findByUsername(username);
+    }
 }
+
 
